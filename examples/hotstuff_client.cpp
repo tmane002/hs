92ee485e0e56f2fc32c8b5f3215167cc5a42b025
tmane002 <tmane002@ucr.edu>
2023-06-02 09:19:30 -0700

    testing multi region

--- examples/hotstuff_client.cpp
+++ examples/hotstuff_client.cpp
@@ -80,15 +80,16 @@ void connect_all()
     cluster_map[2] = 0;
     cluster_map[3] = 0;
     cluster_map[4] = 0;
-    cluster_map[5] = 1;
-    cluster_map[6] = 1;
-    cluster_map[7] = 1;
-    cluster_map[8] = 1;
-    cluster_map[9] = 2;
-    cluster_map[10] = 2;
-    cluster_map[11] = 2;
-    cluster_map[12] = 2;
-    cluster_map[13] = 2;
+    cluster_map[5] = 0;
+    cluster_map[6] = 0;
+    cluster_map[7] = 0;
+    cluster_map[8] = 0;
+    cluster_map[9] = 1;
+    cluster_map[10] = 1;
+    cluster_map[11] = 1;
+    cluster_map[12] = 1;
+    cluster_map[13] = 1;
+
 //    cluster_map[14] = 1;
 //    cluster_map[15] = 1;
 
