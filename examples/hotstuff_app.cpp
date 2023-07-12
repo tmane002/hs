f2cccf7e244de47b36914c57846773a1ad1f66af
tmane002 <tmane002@ucr.edu>
2023-07-12 12:00:37 -0700

    Testing

--- examples/hotstuff_app.cpp
+++ examples/hotstuff_app.cpp
@@ -263,31 +263,52 @@ int main(int argc, char **argv) {
     cluster_map[15] = 3;
 
 
-//    cluster_map[12] = 1;
-//    cluster_map[13] = 1;
-//    cluster_map[9] = 2;
-//    cluster_map[10] = 2;
-//    cluster_map[11] = 2;
+    cluster_map[16] = 4;
+    cluster_map[17] = 4;
+    cluster_map[18] = 4;
+    cluster_map[19] = 4;
 
-//
-//    cluster_map[12] = 2;
-//    cluster_map[13] = 2;
-//    cluster_map[14] = 2;
-//
-//    cluster_map[15] = 3;
-//    cluster_map[16] = 3;
-//    cluster_map[17] = 3;
-//    cluster_map[18] = 3;
-//    cluster_map[19] = 3;
 
-//    cluster_map[20] = 2;
-//    cluster_map[21] = 3;
-//    cluster_map[22] = 3;
-//    cluster_map[23] = 3;
-//    cluster_map[24] = 3;
-//    cluster_map[25] = 3;
-//    cluster_map[26] = 3;
-//    cluster_map[27] = 3;
+    cluster_map[20] = 5;
+    cluster_map[21] = 5;
+    cluster_map[22] = 5;
+    cluster_map[23] = 5;
+
+
+    cluster_map[24] = 6;
+    cluster_map[25] = 6;
+    cluster_map[26] = 6;
+    cluster_map[27] = 6;
+
+
+    cluster_map[28] = 7;
+    cluster_map[29] = 7;
+    cluster_map[30] = 7;
+    cluster_map[31] = 7;
+
+
+    cluster_map[32] = 8;
+    cluster_map[33] = 8;
+    cluster_map[34] = 8;
+    cluster_map[35] = 8;
+
+
+    cluster_map[36] = 9;
+    cluster_map[37] = 9;
+    cluster_map[38] = 9;
+    cluster_map[39] = 9;
+
+
+    cluster_map[40] = 10;
+    cluster_map[41] = 10;
+    cluster_map[42] = 10;
+    cluster_map[43] = 10;
+
+
+    cluster_map[44] = 11;
+    cluster_map[45] = 11;
+    cluster_map[46] = 11;
+    cluster_map[47] = 11;
 
 
 
--- examples/hotstuff_client.cpp
+++ examples/hotstuff_client.cpp
@@ -86,7 +86,7 @@ void connect_all()
     cluster_map[6] = 1;
     cluster_map[7] = 1;
 
-
+    
     cluster_map[8] = 2;
     cluster_map[9] = 2;
     cluster_map[10] = 2;
@@ -99,47 +99,52 @@ void connect_all()
     cluster_map[15] = 3;
 
 
-//    cluster_map[16] = 2;
-//    cluster_map[17] = 2;
+    cluster_map[16] = 4;
+    cluster_map[17] = 4;
+    cluster_map[18] = 4;
+    cluster_map[19] = 4;
 
 
-//    cluster_map[14] = 1;
-//    cluster_map[15] = 1;
-
-//    cluster_map[9] = 2;
-//    cluster_map[10] = 2;
-//    cluster_map[11] = 2;
+    cluster_map[20] = 5;
+    cluster_map[21] = 5;
+    cluster_map[22] = 5;
+    cluster_map[23] = 5;
 
 
-//    cluster_map[12] = 0;
-//    cluster_map[13] = 0;
-//    cluster_map[14] = 0;
-//    cluster_map[15] = 0;
-//    cluster_map[16] = 0;
-//    cluster_map[17] = 0;
-//    cluster_map[18] = 0;
-//    cluster_map[19] = 0;
-//
-//    cluster_map[20] = 1;
-//    cluster_map[21] = 1;
-//    cluster_map[22] = 1;
-//    cluster_map[23] = 1;
-//    cluster_map[24] = 1;
-//    cluster_map[25] = 1;
-//    cluster_map[26] = 1;
-//    cluster_map[27] = 1;
-//    cluster_map[28] = 1;
-//    cluster_map[29] = 1;
-//    cluster_map[30] = 1;
-//    cluster_map[31] = 1;
-//    cluster_map[32] = 1;
-//    cluster_map[33] = 1;
-//    cluster_map[34] = 1;
-//    cluster_map[35] = 1;
-//    cluster_map[36] = 1;
-//    cluster_map[37] = 1;
-//    cluster_map[38] = 1;
-//    cluster_map[39] = 1;
+    cluster_map[24] = 6;
+    cluster_map[25] = 6;
+    cluster_map[26] = 6;
+    cluster_map[27] = 6;
+
+
+    cluster_map[28] = 7;
+    cluster_map[29] = 7;
+    cluster_map[30] = 7;
+    cluster_map[31] = 7;
+
+
+    cluster_map[32] = 8;
+    cluster_map[33] = 8;
+    cluster_map[34] = 8;
+    cluster_map[35] = 8;
+
+
+    cluster_map[36] = 9;
+    cluster_map[37] = 9;
+    cluster_map[38] = 9;
+    cluster_map[39] = 9;
+
+
+    cluster_map[40] = 10;
+    cluster_map[41] = 10;
+    cluster_map[42] = 10;
+    cluster_map[43] = 10;
+
+
+    cluster_map[44] = 11;
+    cluster_map[45] = 11;
+    cluster_map[46] = 11;
+    cluster_map[47] = 11;
 
 
     n_clusters = 0;
