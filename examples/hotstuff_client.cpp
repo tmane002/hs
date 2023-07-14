f1aed5302ffe1661d6d67da9849bb74123a6e88d
tmane002 <tmane002@ucr.edu>
2023-07-14 09:15:33 -0700

    Testing

--- examples/hotstuff_client.cpp
+++ examples/hotstuff_client.cpp
@@ -82,59 +82,50 @@ void connect_all()
     cluster_map[3] = 0;
     cluster_map[4] = 0;
     cluster_map[5] = 0;
+    cluster_map[6] = 0;
+    cluster_map[7] = 0;
+    cluster_map[8] = 0;
+    cluster_map[9] = 0;
+    cluster_map[10] = 0;
+    cluster_map[11] = 0;
+    cluster_map[12] = 0;
+    cluster_map[13] = 0;
+    cluster_map[14] = 0;
+    cluster_map[15] = 0;
+    cluster_map[16] = 0;
+    cluster_map[17] = 0;
+    cluster_map[18] = 0;
+    cluster_map[19] = 0;
+    cluster_map[20] = 0;
+    cluster_map[21] = 0;
+    cluster_map[22] = 0;
+    cluster_map[23] = 0;
 
 
-    cluster_map[6] = 1;
-    cluster_map[7] = 1;
-    cluster_map[8] = 1;
-    cluster_map[9] = 1;
-    cluster_map[10] = 1;
-    cluster_map[11] = 1;
-
-    cluster_map[12] = 2;
-    cluster_map[13] = 2;
-    cluster_map[14] = 2;
-    cluster_map[15] = 2;
-    cluster_map[16] = 2;
-    cluster_map[17] = 2;
-
-    cluster_map[18] = 3;
-    cluster_map[19] = 3;
-    cluster_map[20] = 3;
-    cluster_map[21] = 3;
-    cluster_map[22] = 3;
-    cluster_map[23] = 3;
-
-
-    cluster_map[24] = 4;
-    cluster_map[25] = 4;
-    cluster_map[26] = 4;
-    cluster_map[27] = 4;
-    cluster_map[28] = 4;
-    cluster_map[29] = 4;
-
-    cluster_map[30] = 5;
-    cluster_map[31] = 5;
-    cluster_map[32] = 5;
-    cluster_map[33] = 5;
-    cluster_map[34] = 5;
-    cluster_map[35] = 5;
-
-    cluster_map[36] = 6;
-    cluster_map[37] = 6;
-    cluster_map[38] = 6;
-    cluster_map[39] = 6;
-    cluster_map[40] = 6;
-    cluster_map[41] = 6;
-
-    cluster_map[42] = 7;
-    cluster_map[43] = 7;
-    cluster_map[44] = 7;
-    cluster_map[45] = 7;
-    cluster_map[46] = 7;
-    cluster_map[47] = 7;
-
-
+    cluster_map[24] = 1;
+    cluster_map[25] = 1;
+    cluster_map[26] = 1;
+    cluster_map[27] = 1;
+    cluster_map[28] = 1;
+    cluster_map[29] = 1;
+    cluster_map[30] = 1;
+    cluster_map[31] = 1;
+    cluster_map[32] = 1;
+    cluster_map[33] = 1;
+    cluster_map[34] = 1;
+    cluster_map[35] = 1;
+    cluster_map[36] = 1;
+    cluster_map[37] = 1;
+    cluster_map[38] = 1;
+    cluster_map[39] = 1;
+    cluster_map[40] = 1;
+    cluster_map[41] = 1;
+    cluster_map[42] = 1;
+    cluster_map[43] = 1;
+    cluster_map[44] = 1;
+    cluster_map[45] = 1;
+    cluster_map[46] = 1;
+    cluster_map[47] = 1;
 
     n_clusters = 0;
     for (auto it : cluster_map)
