90dc362b8e56e7f3f1c4f35a5913fc213b6b13a0
tmane002 <tmane002@ucr.edu>
2023-07-25 19:03:45 -0700

    Testing

--- src/consensus.cpp
+++ src/consensus.cpp
@@ -447,6 +447,8 @@ void HotStuffCore::on_init(uint32_t nfaulty) {
     b0->qc_ref = b0;
     hqc = std::make_pair(b0, b0->qc->clone());
 
+
+
         cluster_map[0] = 0;
         cluster_map[1] = 0;
         cluster_map[2] = 0;
@@ -455,30 +457,41 @@ void HotStuffCore::on_init(uint32_t nfaulty) {
         cluster_map[5] = 0;
         cluster_map[6] = 0;
         cluster_map[7] = 0;
-
-        cluster_map[8] = 1;
-        cluster_map[9] = 1;
-        cluster_map[10] = 1;
-        cluster_map[11] = 1;
+        cluster_map[8] = 0;
+        cluster_map[9] = 0;
+        cluster_map[10] = 0;
+        cluster_map[11] = 0;
         cluster_map[12] = 1;
         cluster_map[13] = 1;
         cluster_map[14] = 1;
         cluster_map[15] = 1;
         cluster_map[16] = 1;
         cluster_map[17] = 1;
+        cluster_map[18] = 1;
+        cluster_map[19] = 1;
+        cluster_map[20] = 1;
+        cluster_map[21] = 1;
+        cluster_map[22] = 1;
+        cluster_map[23] = 1;
+        cluster_map[24] = 1;
+        cluster_map[25] = 1;
+        cluster_map[26] = 1;
 
-        cluster_map[18] = 2;
-        cluster_map[19] = 2;
-        cluster_map[20] = 2;
-        cluster_map[21] = 2;
-        cluster_map[22] = 2;
-        cluster_map[23] = 2;
-        cluster_map[24] = 2;
-        cluster_map[25] = 2;
-        cluster_map[26] = 2;
         cluster_map[27] = 2;
-
-
+        cluster_map[28] = 2;
+        cluster_map[29] = 2;
+        cluster_map[30] = 2;
+        cluster_map[31] = 2;
+        cluster_map[32] = 2;
+        cluster_map[33] = 2;
+        cluster_map[34] = 2;
+        cluster_map[35] = 2;
+        cluster_map[36] = 2;
+        cluster_map[37] = 2;
+        cluster_map[38] = 2;
+        cluster_map[39] = 2;
+        cluster_map[40] = 2;
+        cluster_map[41] = 2;
 
 
 
