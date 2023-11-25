151aedca6361e61cdc728ca45d60039688af58ef
tmane002 <tmane002@ucr.edu>
2023-11-25 09:49:45 -0800

    testing older code the commit before this one

--- src/hotstuff.cpp
+++ src/hotstuff.cpp
@@ -1298,14 +1298,14 @@ namespace hotstuff {
                 HOTSTUFF_LOG_INFO("key is %d", key);
 
 
-                if (key%2==1)
-                {
-                    do_decide(Finality(id, 1, 0, 0, cmd_hash, uint256_t()) );
-                }
+//                if (key%2==1)
+//                {
+//                    do_decide(Finality(id, 1, 0, 0, cmd_hash, uint256_t()) );
+//                }
 
                 if (proposer != get_id()) continue;
 
-                if (key%2==0)
+//                if (key%2==0)
                 {
                     cmd_pending_buffer.push(cmd_hash);
 
