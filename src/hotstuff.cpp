9ec53c987cd572f4cc7736d62f5bc5508e0321f5
tejas <tmane002@ucr.edu>
2023-12-19 22:16:28 -0800

    Plot Generation

--- src/hotstuff.cpp
+++ src/hotstuff.cpp
@@ -1438,14 +1438,14 @@ namespace hotstuff {
 //                HOTSTUFF_LOG_INFO("Added to DB successfully");
 
                 
-                if (1>2)//(key%2==1)
+                if (key%100>15)
                 {
                     do_decide_read_only(Finality(id, 1, 0, 0, cmd_hash, uint256_t()), key, val );
                 }
 
                 if (proposer != get_id()) continue;
 
-                if (2>1)//(key%2==0)
+                if (key%100<=15)
                 {
                     cmd_pending_buffer.push(std::make_tuple(cmd_hash, key, val));
                 }
