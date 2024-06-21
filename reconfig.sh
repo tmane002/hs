4b55be8c1712bf67657871a8ce415dd714272221
tejas <tmane002@ucr.edu>
2024-06-21 11:26:33 -0700

    Plot Generation

--- reconfig.sh
+++ reconfig.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-for i in $(seq 1 100)
+for i in $(seq 1 50)
 do
     sudo killall hotstuff-app
+    sleep 0.6
     sudo nohup ./examples/hotstuff-app --conf hotstuff.gen-sec17.conf >../nohup.out 2>&1 &
-    # echo $i
+    echo $i
 done
 
