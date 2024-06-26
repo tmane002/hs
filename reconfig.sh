59c8921b919800734f3c8907e9e133401a6cc222
tejas <tmane002@ucr.edu>
2024-06-26 10:16:13 -0700

    96 node tests

--- reconfig.sh
+++ reconfig.sh
@@ -6,6 +6,7 @@ echo $(date +"%Y-%m-%d %H:%M:%S")
 for i in $(seq 1 180)
 do
     sudo killall hotstuff-app
+    echo $(date +"%Y-%m-%d %H:%M:%S")
     sleep 0.1
     sudo nohup ./examples/hotstuff-app --conf hotstuff.gen-sec17.conf >../nohup.out 2>&1 &
     echo $i
